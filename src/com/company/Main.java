1966655e8ade0eeb793a91777fb3c8e419e182c2
BuFu9 <mads02p8@stud.kea.dk>
2021-10-26 15:36:07 +0200

    Lavet alle pizza

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,20 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 	Scanner input = new Scanner(System.in);
+    Pizza p1 = new Pizza("Vesuvio", "Tomatsauce, ost, skinke, oregano,",57);
+    Pizza p2 = new Pizza("Amerikaner", "Tomatsauce, ost, oksefars, oregano,",53);
+    Pizza p3 = new Pizza("Cacciatore", "Tomatsauce, ost, pepperoni, oregano,",57);
+    Pizza p4 = new Pizza("Carbona", "Tomatsauce, ost, kødsovs, spaghetti,cocktailpølser",63);
+    Pizza p5 = new Pizza("Dennis", "Tomatsauce, ost, skinke, cocktailpølser, oregano,",65);
+    Pizza p6 = new Pizza("Bertil", "Tomatsauce, ost, bacon, oregano,",57);
+    Pizza p7 = new Pizza("Silvia", "Tomatsauce, ost, pepperoni, rød peber,løg,oliven, oregano,",61);
+    Pizza p8 = new Pizza("Victioria", "Tomatsauce, ost, skinke, annanas, løg, champignon,oregano",61);
+    Pizza p9 = new Pizza("Toronfo", "Tomatsauce, ost, skinke, bacon, kebab, chili, oreganno",61);
+    Pizza p10 = new Pizza("Capricciosa", "Tomatsauce, ost, skinke,champignon, oregano,",61);
+    Pizza p11 = new Pizza("Hawai", "Tomatsauce, ost, skinke, annanas, oregano,",61);
+    Pizza p12 = new Pizza("Le Blissoa", "Tomatsauce, ost, skinke, rejer, oregano,",61);
+    Pizza p13 = new Pizza("Venezia", "Tomatsauce, ost, bacon, oregano,",61);
+    Pizza p14 = new Pizza("Mafia", "Tomatsauce, ost, pepperoni, bacon, oregano,",61);
 
     }
 }
