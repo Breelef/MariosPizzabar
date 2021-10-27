a41b1a86af0ce827e09160bc5e689afa26921e5f
breelef <emil_vinther@hotmail.com>
2021-10-27 09:33:11 +0200

    lav pizza metode

--- src/com/company/Pizza.java
+++ src/com/company/Pizza.java
@@ -1,4 +1,5 @@
 package com.company;
+import java.util.*;
 
 public class Pizza {
     String pizzaNavn;
@@ -14,5 +15,14 @@ public class Pizza {
     }
     public String toString(){
         return pizzaNr + ". " + pizzaNavn + ": " + toppings + "............" + pris;
+    }
+    public static Pizza lavPizza(Scanner input){
+        System.out.println("Hvilke toppings skal pizzaen have");
+        String lavToppings = input.nextLine();
+        System.out.println("Indtast pris for pizzaen");
+        int lavPris = input.nextInt();
+        Pizza p1 = new Pizza("Lav selv","tomatsauce, ost, " + lavToppings + ", oregano", lavPris, 15);
+        System.out.println(p1);
+        return p1;
     }
 }
