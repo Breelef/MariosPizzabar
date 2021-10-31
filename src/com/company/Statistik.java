435544a065766caf13bf7e21bf0c0e99c518b2c9
Y0DO <92321437+Y0DO@users.noreply.github.com>
2021-10-31 10:03:36 +0100

    Update Statistik.java

--- src/com/company/Statistik.java
+++ src/com/company/Statistik.java
@@ -1,5 +1,27 @@
 package com.company;
 
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+
 public class Statistik {
+   public void readFile() {
+       try {
+           File myObj = new File("PizzaDerErBlevetBestilt.txt");
+           Scanner myReader = new Scanner(myObj);
+           while (myReader.hasNextLine()) {
+               String data = myReader.nextLine();
+               System.out.println(data);
+           }
+           myReader.close();
+       } catch (
+               FileNotFoundException e) {
+           System.out.println("An error occurred.");
+           e.printStackTrace();
+       }
+   }
 }
 
